63dd46ed4909397c43097b32193566b6a521e32e
Phil Kunz <gitlab@philkunz.com>
2019-10-01 18:06:29 +0200

    fix(core): update

--- test/test.ts
+++ test/test.ts
@@ -1,27 +1,27 @@
 import { expect, tap } from '@pushrocks/tapbundle';
-import * as q from '../ts/index';
+import * as smartpromise from '../ts/index';
 
 tap.test('should return a Deferred for .defer()', async () => {
-  let myDeferred = q.defer();
-  let expectPromise = expect(myDeferred.promise).to.eventually.be.fulfilled;
+  const myDeferred = smartpromise.defer();
+  const expectPromise = expect(myDeferred.promise).to.eventually.be.fulfilled;
   myDeferred.resolve();
   return expectPromise;
 });
 
 tap.test('should let types flow through the Promise', async () => {
-  let myString = 'someString';
-  let myDeferred = q.defer<string>();
-  let expectPromise = expect(myDeferred.promise).to.eventually.equal('someString');
+  const myString = 'someString';
+  const myDeferred = smartpromise.defer<string>();
+  const expectPromise = expect(myDeferred.promise).to.eventually.equal('someString');
   myDeferred.resolve(myString);
   return expectPromise;
 });
 
 tap.test('should map callbacks', async () => {
-  let inputArray = ['hi', 'awesome'];
+  const inputArray = ['hi', 'awesome'];
   const myPromisified = async myInput => {
     return myInput;
   };
-  const expectPromise = expect(q.map(inputArray, myPromisified)).to.eventually.deep.equal(
+  const expectPromise = expect(smartpromise.map(inputArray, myPromisified)).to.eventually.deep.equal(
     inputArray
   );
   return expectPromise;
